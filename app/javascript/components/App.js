85a2cdb2aeca5c527702d3ae3bd41bd89c9910b8
bundaberg <bundaberg@bundabergs-MacBook-Pro.local>
2020-02-11 20:50:04 -0700

    Added greeting Hello Bob for testing persistent state at App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,9 +2,18 @@
 import React, {Component} from 'react'
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state={name: ''};
+  }
+  
   render() {
     return (
-      <div>This is my new app! Yeah!</div>
+      <div>
+        This is my new app! Yeah!...
+        <button onClick={()=>{this.setState({name: 'Bob'});}}>Click to Say Hi</button>
+        <h1>Hello {this.state.name}</h1>
+      </div>
     )
   }
 }
