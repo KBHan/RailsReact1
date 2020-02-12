957aec0055e32561ced932691342bce4465f57e8
bundaberg <bundaberg@bundabergs-MacBook-Pro.local>
2020-02-11 21:37:58 -0700

    Greeting component added and App modified to import Greeting component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,14 @@
 
 import React, {Component} from 'react'
+import Greeting from './Greeting'
 
 class App extends Component {
-  constructor (props) {
-    super(props);
-    this.state={name: ''};
-  }
+  
   
   render() {
     return (
       <div>
-        This is my new app! Yeah!...
-        <button onClick={()=>{this.setState({name: 'Bob'});}}>Click to Say Hi</button>
-        <h1>Hello {this.state.name}</h1>
+        <Greeting/>
       </div>
     )
   }
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,21 @@
+
+import React, {Component} from 'react'
+
+class Greeting extends Component {
+  constructor (props) {
+    super(props);
+    this.state={name: ''};
+  }
+  
+  render() {
+    return (
+      <div>
+        This is my new app! Yeah!...
+        <button onClick={()=>{this.setState({name: 'Bob'});}}>Click to Say Hi</button>
+        <h1>Hello {this.state.name}</h1>
+      </div>
+    )
+  }
+}
+
+export default Greeting
