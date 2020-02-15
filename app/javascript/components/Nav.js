ab6146ffdaa93d4c31e6ef7894afedc612049dc0
bundaberg <bundaberg@bundabergs-MacBook-Pro.local>
2020-02-14 20:59:15 -0700

    Additional components: About, and Nav

---
+++ app/javascript/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Nav(){
+    return(
+        <nav>
+            <h3>Logo</h3>
+            <ul>
+                <li>About</li>
+                <li>Greeting</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
