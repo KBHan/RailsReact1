eb43bbbd1f27d81466a33442e5073a85d0bb6948
bundaberg <bundaberg@bundabergs-MacBook-Pro.local>
2020-02-14 21:34:32 -0700

    Route set up successfully

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,17 +2,29 @@
 import React, {Component} from 'react';
 import Greeting from './Greeting';
 import Nav from './Nav';
+import About from './About';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  
-  
   render() {
     return (
       <div>
-        <Nav/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Greeting" component={Greeting} />
+          <Route exact path="/Nav" component={Nav} />
+        </Switch>
       </div>
     )
   }
 }
+
+const Home = function(){
+  return (
+    <h3>Home Page</h3>
+  )
+}
+
 
 export default App
